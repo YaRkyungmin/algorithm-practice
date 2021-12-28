efba9ca74a75c2a77a02087b7ca6511199503296
KANGKYUNGMIN <kkgm94@naver.com>
2021-12-29 00:41:06 +0900

    2021-12-28

---
+++ lottos of min max.py
@@ -0,0 +1,17 @@
+# autor: YaRkyungmin
+# 2021-12-28
+# https://programmers.co.kr/learn/courses/30/lessons/77484?language=python3#fnref1
+
+def solution(lottos, win_nums):
+    answer = []
+    dic_win = {0:6,1:6,2:5,3:4,4:3,5:2,6:1}
+    correct_count = 0
+    zero_count = 0
+    for i in range(6):
+        if lottos[i] in win_nums:
+            correct_count += 1
+        elif lottos[i] == 0:
+            zero_count += 1
+    max_count = correct_count+zero_count
+    answer = [dic_win[max_count],dic_win[correct_count]]
+    return answer

274cce7ca03a328bfc9f5f4c1f3a43bf48fa3c3e
YaRkyungmin <kkgm94@naver.com>
2023-06-12 02:39:06 +0900

    🚚build: 숫자의 합 구하기, 평균 구하기 파일 추가

---
+++ BaekJoon/배열과 리스트/숫자의 합 구하기.py
@@ -0,0 +1,7 @@
+"""
+author: kyungmin
+date: 23.06.12
+title: 숫자의 합 구하기
+"""
+import sys
+input = sys.stdin.readline
---
+++ BaekJoon/배열과 리스트/평균 구하기.py
@@ -0,0 +1,7 @@
+"""
+author: kyungmin
+date: 23.06.12
+title: 평균 구하기
+"""
+import sys
+input = sys.stdin.readline

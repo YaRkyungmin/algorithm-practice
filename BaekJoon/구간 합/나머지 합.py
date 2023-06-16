576c2ee7f2ac3511ee6a0ad8af8d7693a04cfc9e
YaRkyungmin <kkgm94@naver.com>
2023-06-16 15:11:53 +0900

    ⚡️review: 나머지 합

--- BaekJoon/구간 합/나머지 합.py
+++ BaekJoon/구간 합/나머지 합.py
@@ -10,21 +10,21 @@ N, M = map(int, input().split())
 
 a = list(map(int, input().split()))
 s_a = [0 for i in range(N)]
-##
+
 s = 0
 for i in range(N):
     s += a[i]
     s_a[i] = s
 
-result = 0
+s = 0
 s_a_r = [i%M for i in s_a]
 r_array = [0 for i in range(M)]
 
 for i in range(N):
     r_array[s_a_r[i]] += 1
 
-result += r_array[0]
+s += r_array[0]
 for i in r_array:
     if i > 1:
-        result += int(i*(i-1)/2) 
-print(result)
+        s += int(i*(i-1)/2) 
+print(s)

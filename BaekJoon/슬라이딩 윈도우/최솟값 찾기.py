1eda6e185e7b3a075452860650fe6a682317120b
YaRkyungmin <kkgm94@naver.com>
2023-06-22 22:06:40 +0900

    ⚡️review: 최솟값 찾기

--- BaekJoon/슬라이딩 윈도우/최솟값 찾기.py
+++ BaekJoon/슬라이딩 윈도우/최솟값 찾기.py
@@ -4,21 +4,17 @@ date: 23.06.19
 title: 최솟값 찾기
 time: 30분
 """
+# 1 5 2 3 6 2 3 7 3 5 2 6
+from collections import deque
 N, L = map(int, input().split())
 arr = list(map(int, input().split()))
-minValue = arr[0]
-result = f"{minValue}"
-
-for i in range(1, L):
-    if arr[i] < minValue:
-        minValue = arr[i]
-    result += f" {minValue}"
-
-for i in range(L, N):
-    if arr[i] <= minValue:
-        minValue = arr[i]
-    elif arr[i-L] == minValue:
-        minValue = min(arr[i-L+1:i+1])
-    result += f" {minValue}"
-print(result)
-
+d = deque()
+result = []
+for i in range(N):
+    while d and d[-1][1] >= arr[i]:
+        d.pop()
+    d.append((i,arr[i]))
+    if d[0][0] == i - L :
+        d.popleft()
+    result.append(d[0][1])
+print(*result)

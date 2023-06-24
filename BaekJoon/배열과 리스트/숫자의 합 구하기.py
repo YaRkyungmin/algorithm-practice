a92eea4ef4d8c30b1069f950e7c4bee1e1664b19
YaRkyungmin <kkgm94@naver.com>
2023-06-24 12:37:52 +0900

    ⚡️review: 숫자의 합 구하기

--- BaekJoon/배열과 리스트/숫자의 합 구하기.py
+++ BaekJoon/배열과 리스트/숫자의 합 구하기.py
@@ -4,10 +4,12 @@ date: 23.06.12
 title: 숫자의 합 구하기
 time: 15분
 """
-i = int(input())
+import sys
+input = sys.stdin.readline
+N = int(input())
 
-a = list(input())
-
-for z in a :
-    sum += int(z)
+a = input()
+sum = 0
+for z in range(N) :
+    sum += int(a[z])
 print(sum)

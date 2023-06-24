290823184fd328fe18e32d6b4b37c78d6b90a947
YaRkyungmin <kkgm94@naver.com>
2023-06-24 12:59:16 +0900

    ⚡️review: 오큰수

--- BaekJoon/스택과 큐/오큰수.py
+++ BaekJoon/스택과 큐/오큰수.py
@@ -4,11 +4,17 @@ date: 23.06.21
 title: 오큰수
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-arr = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
+
+result = [-1 for _ in range(N)]
 stack = []
-r = [-1 for i in range(N)]
-
-
-    
-
+for i in range(N):
+    while stack and numbers[i] > numbers[stack[-1]] :
+        node_index = stack.pop()
+        result[node_index] = numbers[i]
+    stack.append(i)
+print(*result)

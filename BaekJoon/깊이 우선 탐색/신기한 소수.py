cf76ac0701bfb8dd6bd08557727ce1d946b41c53
YaRkyungmin <kkgm94@naver.com>
2023-07-03 23:41:08 +0900

    ⚡️review: 신기한 소수

--- BaekJoon/깊이 우선 탐색/신기한 소수.py
+++ BaekJoon/깊이 우선 탐색/신기한 소수.py
@@ -4,28 +4,9 @@ date: 23.06.25
 title: 신기한 소수
 time: 30분
 """
-import sys
-input = sys.stdin.readline
+testList = [1,2,3]
+test = list(testList)
 
-N = int(input())
-
-def isPrime(num):
-    for i in range(2, int(num / 2 + 1)):
-        if num % i == 0:
-            return False
-    return True
-
-def DFS(number):
-    if len(str(number)) == N:
-        print(number)
-    else:
-        for i in range(1, 10):
-            if i % 2 == 0:
-                continue
-            if isPrime(number * 10 + i):
-                DFS(number * 10 + i)
-
-DFS(2)
-DFS(3)
-DFS(5)
-DFS(1)
+test[2] = 1
+print(testList)
+print(test)

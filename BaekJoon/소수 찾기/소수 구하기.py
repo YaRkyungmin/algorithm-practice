a12953b145d97d66533bd5e0719689510421c519
YaRkyungmin <kkgm94@naver.com>
2023-07-05 23:44:39 +0900

    ⚡️review: 소수 구하기

--- BaekJoon/소수 찾기/소수 구하기.py
+++ BaekJoon/소수 찾기/소수 구하기.py
@@ -8,14 +8,33 @@ import sys
 input = sys.stdin.readline
 
 M, N = map(int, input().split())
-true_false_list = [True] * (N+1)
-true_false_list[1] = False
 
-for i in range(2, int(N**0.5) + 1):
-    if true_false_list[i] == True:
-        for j in range(2 * i, N+1, i):
-            true_false_list[j] = False
+prime_numbers = []
+
+def isPrime(num):
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 for i in range(M, N+1):
-    if true_false_list[i]:
+    if i == 1:
+        continue
+    if isPrime(i):
         print(i)
+
+# import sys
+# input = sys.stdin.readline
+
+# M, N = map(int, input().split())
+# true_false_list = [True] * (N+1)
+# true_false_list[1] = False
+
+# for i in range(2, int(N**0.5) + 1):
+#     if true_false_list[i] == True:
+#         for j in range(2 * i, N+1, i):
+#             true_false_list[j] = False
+
+# for i in range(M, N+1):
+#     if true_false_list[i]:
+#         print(i)

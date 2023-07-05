ad30193e4d667b363472c9dcd929a06e03be3133
YaRkyungmin <kkgm94@naver.com>
2023-07-06 01:06:30 +0900

    ❌unsolved: K번째 수

---
+++ BaekJoon/이진 탐색/K번째 수.py
@@ -0,0 +1,21 @@
+"""
+author: kyungmin
+date: 23.07.06
+title: K번째 수
+time: 30분
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+
+# 1 2 3 4 5
+# 2 4 6 8 10
+# 3 6 9 12 15
+# 4 8 12 16 20
+# 5 10 15 20 25
+
+# 1 2 2 3 3 4 4 4 5 5 6 6 8 8 9
+
+# 10 000 000 000

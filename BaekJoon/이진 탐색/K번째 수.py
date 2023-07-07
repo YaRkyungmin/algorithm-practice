74435192c905e870fb039eac0a9a6aae6d0d6a9b
YaRkyungmin <kkgm94@naver.com>
2023-07-07 23:55:35 +0900

    ⚡️review: K번째 수

--- BaekJoon/이진 탐색/K번째 수.py
+++ BaekJoon/이진 탐색/K번째 수.py
@@ -8,14 +8,34 @@ import sys
 input = sys.stdin.readline
 
 N = int(input().rstrip())
+K = int(input().rstrip())
+start = 1
+end = N * N
+result = 1
+while start < end:
+    mid = (start + end) // 2
+    sum_num = 0
+    for i in range(1, N + 1):
+        cnt = mid // i
+        if cnt > N:
+            sum_num += N
+        else:
+            sum_num += cnt
+    if sum_num < K:
+        start = mid + 1
+    elif sum_num > K:
+        end = mid - 1
+    else:
+        result = mid
+        break
+if start >= end:
+    print(start)
+else:
+    print(result)
+# 1 2 3 3
+# 2 4 6 3
+# 3 6 9 2
 
+# 1 2 2 3 3 4 6 6 9
 
-# 1 2 3 4 5
-# 2 4 6 8 10
-# 3 6 9 12 15
-# 4 8 12 16 20
-# 5 10 15 20 25
-
-# 1 2 2 3 3 4 4 4 5 5 6 6 8 8 9
-
-# 10 000 000 000
+# 1 2 3 4 5m 6s 7m 8 9e
--- README.md
+++ README.md
@@ -40,5 +40,5 @@
 |6월 29일| [너비 우선 탐색 - 트리의 지름](https://www.acmicpc.net/problem/1167) |❌|✅❌|
 |7월 1일| [이진 탐색 - 수 찾기](https://www.acmicpc.net/problem/1920) |❌|✅|
 |7월 4일| [이진 탐색 - 기타 레슨](https://www.acmicpc.net/problem/2343) |❌|✅❌|
-|7월 6일| [이진 탐색 - K번째 수](https://www.acmicpc.net/problem/1300) |❌|✅❌|
+|7월 6일| [이진 탐색 - K번째 수](https://www.acmicpc.net/problem/1300) |❌|✅|
 |7월 6일| [그리디 - 동전 0](https://www.acmicpc.net/problem/11047) |✅❌|✅❌|

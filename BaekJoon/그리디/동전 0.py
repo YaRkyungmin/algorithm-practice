c74504e8e216e3d10cd115dd025874ddfe1745e9
YaRkyungmin <kkgm94@naver.com>
2023-07-09 23:52:09 +0900

    ❌unsolved: 동전 0 2nd

--- BaekJoon/그리디/동전 0.py
+++ BaekJoon/그리디/동전 0.py
@@ -1,8 +1,26 @@
 """
 author: kyungmin
-date: 23.07.08
+date: 23.07.09
 title: 동전 0
 time: 30분
 """
 import sys
-input = sys.stdin.readlines
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+numbers = []
+for _ in range(N):
+    numbers.append(int(input().rstrip()))
+num_sum = 0
+cnt = 0 
+
+for i in range(-1, -(N + 1), -1):
+    if numbers[i] <= K:
+        while num_sum <= K:
+            num_sum += numbers[i]
+            cnt += 1
+        num_sum -= numbers[i]
+        cnt -= 1
+    if num_sum == K:
+        break
+print(cnt)

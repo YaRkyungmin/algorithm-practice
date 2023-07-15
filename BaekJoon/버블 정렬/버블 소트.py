42250d07c4b02a39d37a05804dea71bf9e29bbde
YaRkyungmin <kkgm94@naver.com>
2023-07-16 00:27:38 +0900

    ⚡️review: 버블 소트

--- BaekJoon/버블 정렬/버블 소트.py
+++ BaekJoon/버블 정렬/버블 소트.py
@@ -11,4 +11,8 @@ N = int(input().rstrip())
 numbers = [(int(input()), i) for i in range(N)]
 numbers.sort()
 
-
+max = 0
+for i in range(N):
+    if max < numbers[i][1] - i:
+        max = numbers[i][1] - i
+print(max+1)

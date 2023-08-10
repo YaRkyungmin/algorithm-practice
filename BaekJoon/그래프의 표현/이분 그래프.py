c09920f2b4ef9ee3c4d28283d3b518ec834669eb
YaRkyungmin <kkgm94@naver.com>
2023-08-11 01:45:01 +0900

    ⚡️review: 이분 그래프

--- BaekJoon/그래프의 표현/이분 그래프.py
+++ BaekJoon/그래프의 표현/이분 그래프.py
@@ -9,3 +9,65 @@ from collections import deque
 input = sys.stdin.readline
 
 K = int(input().rstrip())
+
+def change(color):
+    if color == 0:
+        return 1
+    else:
+        return 0
+result_list = []
+
+for _ in range(K):
+    V, E = map(int, input().rstrip().split())
+
+    grahp = [[] for _ in range(V + 1)]
+
+    for _ in range(E):
+        u, v = map(int, input().rstrip().split())
+        grahp[u].append(v)
+        grahp[v].append(u)
+
+    queue = deque()
+
+    color_list = [0 for _ in range(V + 1)]
+    visit = [False] * (V + 1)
+    
+    def BFS(node):
+        node_linked_list = grahp[node]
+        node_count = len(node_linked_list)
+
+        for i in range(node_count):
+            queue.append((node_linked_list[i], color_list[node]))
+        
+        while queue:
+            (pop_node, pop_color) = queue.popleft()
+            pop_node_linked_list = grahp[pop_node]
+            pop_node_count = len(pop_node_linked_list)
+            append_color = change(pop_color)
+
+            for i in range(pop_node_count):
+                append_node = pop_node_linked_list[i]
+
+                if visit[append_node] == False:
+                    visit[append_node] = True
+                    queue.append((append_node, append_color))
+                    color_list[append_node] = append_color
+                else:
+                    if color_list[append_node] != append_color:
+                        return 1
+        return 0
+    
+    last_result = 0
+    for i in range(1, V+1):
+        if last_result == 0:
+            if visit[i] == False:
+                last_result = BFS(i)
+        else:
+            break
+    
+    if last_result == 0:
+        result_list.append("YES")
+    else:
+        result_list.append("NO")
+
+print(*result_list, sep="\n")

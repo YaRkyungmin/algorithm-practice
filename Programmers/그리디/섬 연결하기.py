312381b38b99a5d4ebcd08802ab00dfc2d0010c9
YaRkyungmin <kkgm94@naver.com>
2023-09-09 17:37:03 +0900

    ❌unsolved: 섬 연결하기

--- Programmers/그리디/섬 연결하기.py
+++ Programmers/그리디/섬 연결하기.py
@@ -4,3 +4,15 @@ date: 23.09.08
 title: 섬 연결하기
 time: 30분
 """
+def solution(n, costs):
+    answer = 0
+    dp = [False] * n
+    costs.sort(key = lambda x: x[2]) 
+
+    for i in costs:
+        if dp[i[0]] and dp[i[1]]:
+            continue
+        dp[i[0]] = True
+        dp[i[1]] = True
+        answer += i[2]
+    return answer

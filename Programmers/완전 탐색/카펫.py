35f9d053b769f0e3fc21bd8402e8aa82609682ca
YaRkyungmin <kkgm94@naver.com>
2023-09-12 17:41:18 +0900

    ✅solved: 카펫

--- Programmers/완전 탐색/카펫.py
+++ Programmers/완전 탐색/카펫.py
@@ -4,3 +4,23 @@ date: 23.09.13
 title: 카펫
 time: 30분
 """
+
+def solution(brown, yellow):
+    answer = []
+    kaffet_count = brown + yellow
+    array_list = []
+    for i in range(1, int(kaffet_count**.5) + 1):
+        if kaffet_count % i == 0:
+            a = kaffet_count // i
+            b = i
+            if a < b:
+                tmp = a
+                a = b
+                b = tmp
+            array_list.append([a, b])
+    while array_list:
+        [a, b] = array_list.pop()
+        if (a-2) * (b-2) == yellow:
+            answer = [a, b]
+            break       
+    return answer

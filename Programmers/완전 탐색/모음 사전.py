6f424b761ed695aa0405dc1c8a281f0605def964
YaRkyungmin <kkgm94@naver.com>
2023-09-27 11:31:30 +0900

    ❌unsolved: 모음 사전

--- Programmers/완전 탐색/모음 사전.py
+++ Programmers/완전 탐색/모음 사전.py
@@ -4,3 +4,22 @@ date: 23.09.30
 title: 모음 사전
 time: 30분
 """
+order = 0    
+dic = {}
+lst =["A","E","I","O","U"] 
+
+def solution(word):
+    answer = 0
+    dfs("")
+    return dic[word]
+
+def dfs(s):
+    global order, dic, lst
+    if len(s) > 5:
+        return
+    dic[s] = order
+    order += 1
+    for i in lst:
+        if(len(s+i) > 5):
+            return
+        dfs(s + i)

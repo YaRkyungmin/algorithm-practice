7bf632408e8ae67f999ce8d918bbb9b418bc28b1
YaRkyungmin <kkgm94@naver.com>
2023-10-05 12:52:28 +0900

    ✅solved: 방 번호

--- BaekjoonWithSwift/BaekjoonWithSwift/배열/방번호.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/배열/방번호.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 func 방번호() {
-    
+    let input = readLine() ?? "0"
+
+    var array = Array(repeating: 0, count: 10)
+
+    for i in input {
+        if i == "9" {
+            if array[9] > array[6] {
+                array[6] += 1
+            } else {
+                array[9] += 1
+            }
+        } else if i == "6" {
+            if array[9] < array[6] {
+                array[9] += 1
+            } else {
+                array[6] += 1
+            }
+        } else {
+            array[Int(String(i)) ?? 0] += 1
+        }
+    }
 }

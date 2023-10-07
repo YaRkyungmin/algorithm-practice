ab2768cf877079bb86a0fb7527086bae4a7138b0
YaRkyungmin <kkgm94@naver.com>
2023-10-07 12:57:19 +0900

    ✅solved: 두 수의 합

--- BaekjoonWithSwift/BaekjoonWithSwift/배열/두수의합.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/배열/두수의합.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 func 두수의합() {
-    
+    let N = Int(String(readLine()!))!
+    var array = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
+    let x = Int(String(readLine()!))!
+
+    var setset = Set<Int>()
+
+    for i in array {
+        setset.insert(i)
+    }
+
+    var answer = 0
+
+    for i in array {
+        if setset.contains(x - i) {
+            answer += 1
+        }
+    }
+    print(answer/2)
 }

cb9c0db14b4ddc0ff66258e3c23eb70fd42282de
YaRkyungmin <kkgm94@naver.com>
2023-10-15 13:17:08 +0900

    ✅solved: 스택

--- BaekjoonWithSwift/BaekjoonWithSwift/스택/스택.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/스택/스택.swift
@@ -8,5 +8,37 @@
 import Foundation
 
 func 스택() {
-    
+    let N = Int(readLine()!)!
+    var stack: [String] = []
+
+    for _ in 0..<N {
+        let order = readLine()!.split(separator: " ").compactMap { String($0) }
+        
+        switch order[0] {
+        case "push":
+            stack.append(order[1])
+        case "top":
+            if stack.isEmpty == false {
+                print(stack.last!)
+            } else {
+                print("-1")
+            }
+        case "size":
+            print(stack.count)
+        case "empty":
+            if stack.isEmpty {
+                print("1")
+            } else {
+                print("0")
+            }
+        case "pop":
+            if stack.isEmpty == false {
+                print(stack.removeLast())
+            } else {
+                print("-1")
+            }
+        default:
+            print("", terminator: "")
+        }
+    }
 }

cf2feb0e9e94844effa00ace6d29d91fcccd52f5
YaRkyungmin <kkgm94@naver.com>
2023-10-17 13:19:39 +0900

    ✅solved: 제로

--- BaekjoonWithSwift/BaekjoonWithSwift/스택/제로.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/스택/제로.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 func 제로() {
-    
+    let N = Int(readLine()!)!
+    var stack: [Int] = []
+
+    for _ in 0..<N {
+        let input = Int(readLine()!)!
+        
+        if input == 0 {
+            stack.removeLast()
+        } else {
+            stack.append(input)
+        }
+    }
+
+    print(stack.reduce(0) { $0 + $1 })
 }

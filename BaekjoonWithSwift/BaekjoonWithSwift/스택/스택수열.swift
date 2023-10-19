c4bc8b0fda35f8324efb59b8e7f5cb747ae1d779
YaRkyungmin <kkgm94@naver.com>
2023-10-19 13:22:52 +0900

    ✅solved: 스택 수열

--- BaekjoonWithSwift/BaekjoonWithSwift/스택/스택수열.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/스택/스택수열.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 func 스택수열() {
-    
+    let N = Int(readLine()!)!
+    var number = 0
+    var stack: [Int] = []
+    var output = ""
+
+    for _ in 0..<N {
+        let input = Int(readLine()!)!
+        
+        while number < input {
+            number += 1
+            stack.append(number)
+            output += "+"
+        }
+        
+        if input == stack.removeLast() {
+            output += "-"
+        } else {
+            output = "NO"
+            break
+        }
+    }
+
+    if output == "NO" {
+        print(output)
+    } else {
+        output.forEach { print($0) }
+    }
 }
--- README.md
+++ README.md
@@ -98,4 +98,4 @@
 |10월 12일| [연결 리스트 - 요세푸스 문제](https://www.acmicpc.net/problem/1158) |✅||
 |10월 14일| [스택 - 스택](https://www.acmicpc.net/problem/10828) |✅||
 |10월 16일| [스택 - 제로](https://www.acmicpc.net/problem/10773) |✅||
-|10월 18일| [스택 - 스택 수열](https://www.acmicpc.net/problem/1874) |✅❌|✅❌|
+|10월 18일| [스택 - 스택 수열](https://www.acmicpc.net/problem/1874) |✅||

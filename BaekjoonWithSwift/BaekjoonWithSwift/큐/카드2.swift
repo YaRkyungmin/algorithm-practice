7442baf9dfe984c13b3aeb81abb79541148efa9a
YaRkyungmin <kkgm94@naver.com>
2023-10-28 14:00:00 +0900

    ✅solved: 카드2

--- BaekjoonWithSwift/BaekjoonWithSwift/큐/카드2.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/큐/카드2.swift
@@ -8,5 +8,46 @@
 import Foundation
 
 func 카드2() {
-    
+    let N = Int(readLine()!)!
+    var head: Node?
+    var tail: Node?
+    var size = 0
+
+    for i in 1...N {
+        let node = Node(i)
+        
+        if head == nil {
+            head = node
+            tail = node
+            size += 1
+        } else {
+            tail?.next = node
+            tail = node
+            size += 1
+        }
+    }
+
+    while size > 1 {
+        head = head?.next
+        size -= 1
+        
+        if size == 1 {
+            break
+        }
+        
+        tail?.next = head
+        head = head?.next
+        tail = tail?.next
+    }
+
+    print(head!.value)
+
+    class Node {
+        var next: Node?
+        let value: Int
+
+        init(_ value: Int) {
+            self.value = value
+        }
+    }
 }

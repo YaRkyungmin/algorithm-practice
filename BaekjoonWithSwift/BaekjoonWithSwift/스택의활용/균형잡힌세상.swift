a24134fd33a7ffa54468ad85f140cd3744db21f5
YaRkyungmin <kkgm94@naver.com>
2023-11-14 16:40:16 +0900

    ❌unsolved: 균형잡힌 세상

--- BaekjoonWithSwift/BaekjoonWithSwift/스택의활용/균형잡힌세상.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/스택의활용/균형잡힌세상.swift
@@ -8,5 +8,52 @@
 import Foundation
 
 func 균형잡힌세상() {
-    
+    while true {
+        var input = readLine()!
+        var stack1 = [String]()
+        var signal = true
+        
+        if input == "." {
+            break
+        }
+        
+        while input.count > 0 {
+            let ele = String(input.popLast()!)
+            switch ele {
+            case "(":
+                if stack1.count > 0 {
+                    if String(stack1.popLast()!) != ")" {
+                        signal = false
+                        break
+                    }
+                } else {
+                    signal = false
+                    break
+                }
+            case ")":
+                stack1.append(ele)
+            case "[":
+                if stack1.count > 0 {
+                    if String(stack1.popLast()!) != "]" {
+                        signal = false
+                        break
+                    }
+                } else {
+                    signal = false
+                    break
+                }
+            case "]":
+                stack1.append(ele)
+                
+            default:
+                continue
+            }
+        }
+        
+        if stack1.count > 0 {
+            signal = false
+        }
+        
+        print(signal ? "yes" : "no")
+    }
 }

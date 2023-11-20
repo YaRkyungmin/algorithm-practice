743a9f2973430c6eb0c9be7187b5128efb31d97f
YaRkyungmin <kkgm94@naver.com>
2023-11-20 20:52:03 +0900

    ✅solved: 쇠막대기

--- BaekjoonWithSwift/BaekjoonWithSwift/스택의활용/쇠막대기.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/스택의활용/쇠막대기.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 func 쇠막대기() {
-    
+    var count = 0
+    var stack = 0
+    let input = readLine()!.compactMap { String($0) }
+    var signal = false
+    var pre_count = 0
+    for i in (0..<input.count).reversed() {
+        switch input[i] {
+        case "(":
+            if signal {
+                stack -= 1
+                count += stack - pre_count
+                pre_count = 0
+                signal = false
+            } else {
+                count += stack - pre_count
+                stack -= 1
+                pre_count = stack
+            }
+        case ")":
+            stack += 1
+            signal = true
+        default:
+            continue
+        }
+    }
+    print(count)
 }
--- README.md
+++ README.md
@@ -110,7 +110,7 @@
 |11월 14일| [스택의 활용(수식의 괄호 쌍) - 균형잡힌 세상](https://www.acmicpc.net/problem/4949) |❌|✅❌|
 |11월 16일| [스택의 활용(수식의 괄호 쌍) - 좋은 단어](https://www.acmicpc.net/problem/3986) |✅||
 |11월 18일| [스택의 활용(수식의 괄호 쌍) - 괄호](https://www.acmicpc.net/problem/9012) |✅||
-|11월 20일| [스택의 활용(수식의 괄호 쌍) - 쇠막대기](https://www.acmicpc.net/problem/10799) |✅❌|✅❌|
+|11월 20일| [스택의 활용(수식의 괄호 쌍) - 쇠막대기](https://www.acmicpc.net/problem/10799) |✅||
 
 ### KakaoInternship ✨
 | date       | problem set          | solve | review |

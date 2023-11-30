c976c3ed0c2881e30a7ed29b77ec278f0ca6acb1
YaRkyungmin <kkgm94@naver.com>
2023-12-01 03:41:50 +0900

    ❌unsolved: 불!

--- BaekJoon/BFS/불!.py
+++ BaekJoon/BFS/불!.py
@@ -4,3 +4,44 @@ date: 23.11.30
 title: 불!
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+R, C = map(int, input().split())
+
+map = []
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+for _ in range(R):
+    map.append(list(input().rstrip()))
+queue = deque()
+for y in range(R):
+    for x in range(C):
+        if map[y][x] == "F":
+            queue.append([x, y, "F", 1])
+        elif map[y][x] == "J":
+            queue.append([x, y, "J", 1])
+        else:
+            continue
+
+result = 0
+signal = False
+while queue:
+    [x, y, who, time] = queue.popleft()
+
+    if who == "J":
+        if x == 0 or x == C - 1 or y == 0 or y == R - 1:
+            result = time
+            signal = True
+            break
+    for i in range(4):
+        if 0 <= x + dx[i] < C and 0 <= y + dy[i] < R and map[y + dy[i]][x + dx[i]] == ".":
+            map[y + dy[i]][x + dx[i]] = who
+            queue.append([x + dx[i], y + dy[i], who, time + 1])
+
+if signal:
+    print(result)
+else:
+    print("IMPOSSIBLE")
+    

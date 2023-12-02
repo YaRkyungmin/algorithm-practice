6ed0fa0ef2dc2afcf5424513bd6d1cc95616d161
YaRkyungmin <kkgm94@naver.com>
2023-12-02 12:58:53 +0900

    ❌unsolved: 숨바꼭질

--- BaekJoon/BFS/숨바꼭질.py
+++ BaekJoon/BFS/숨바꼭질.py
@@ -4,3 +4,26 @@ date: 23.12.01
 title: 숨바꼭질
 time: 30분
 """
+import sys
+from collections import deque
+N, K = map(int, input().rstrip().split())
+queue = deque()
+queue.append((N,0))
+result = 0
+visit = [0 for _ in range(100003)] # N이 K보다 작을 때를 생각해야함
+while queue:
+    n_point, time = queue.popleft()
+    if n_point == K:
+        result = time
+        break
+    visit[n_point] = 1
+    if n_point - 1 >= 0 and visit[n_point-1] == 0:
+        queue.append((n_point - 1, time + 1))
+    if n_point + 1 <= K + 2 and visit[n_point+1] == 0:
+        queue.append((n_point + 1, time + 1))
+    if n_point * 2 <= K + 2 and visit[n_point*2] == 0:
+        queue.append((n_point * 2, time + 1))
+
+
+print(result)
+

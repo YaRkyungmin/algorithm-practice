c2c32585f69938e1a590e4f8ab9d0f8d95e8b88d
YaRkyungmin <kkgm94@naver.com>
2023-12-02 13:16:53 +0900

    ⚡️review: 숨바꼭질

--- BaekJoon/BFS/숨바꼭질.py
+++ BaekJoon/BFS/숨바꼭질.py
@@ -8,22 +8,23 @@ import sys
 from collections import deque
 N, K = map(int, input().rstrip().split())
 queue = deque()
-queue.append((N,0))
+queue.append(N)
 result = 0
 visit = [0 for _ in range(100003)] # N이 K보다 작을 때를 생각해야함
 while queue:
-    n_point, time = queue.popleft()
+    n_point = queue.popleft()
     if n_point == K:
-        result = time
+        result = visit[n_point]
         break
-    visit[n_point] = 1
     if n_point - 1 >= 0 and visit[n_point-1] == 0:
-        queue.append((n_point - 1, time + 1))
+        queue.append(n_point - 1)
+        visit[n_point - 1] = visit[n_point] + 1
     if n_point + 1 <= K + 2 and visit[n_point+1] == 0:
-        queue.append((n_point + 1, time + 1))
+        queue.append(n_point + 1)
+        visit[n_point + 1] = visit[n_point] + 1
     if n_point * 2 <= K + 2 and visit[n_point*2] == 0:
-        queue.append((n_point * 2, time + 1))
-
+        queue.append(n_point * 2)
+        visit[n_point * 2] = visit[n_point] + 1
 
 print(result)
 

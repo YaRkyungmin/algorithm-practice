924b2d5a132739ceedece68fd5a7fbb456e2956e
YaRkyungmin <kkgm94@naver.com>
2023-12-04 03:28:16 +0900

    ⚡️review: 적록색약

--- BaekJoon/BFS/적록색약.py
+++ BaekJoon/BFS/적록색약.py
@@ -5,54 +5,48 @@ title: 적록색약
 time: 30분
 """
 
-import sys
 from collections import deque
 
-input = sys.stdin.readline
+N = int(input())
+picture = [list(input()) for _ in range(N)]
 
-N = int(input().rstrip())
-picture = []
-
-for _ in range(N):
-    row = list(input().rstrip())
-    picture.append(row)
-
-visit = [[0 for _ in range(N)] for _ in range(N)]
+visit = [[0] * N for _ in range(N)]
 o_count = 0
 x_count = 0
 
 dx = [-1, 1, 0 , 0]
 dy = [0, 0, -1, 1]
-queue = deque()
+def bfs(x, y):
+    queue = deque()
+    c_color = picture[y][x]
+    queue.append([x, y])
+    visit[y][x] = 1 # 어디서 visit을 처리해주는지가 아주 중요
+    while queue:
+        nx, ny = queue.popleft()
+        for i in range(4):
+            if 0 <= ny + dy[i] < N and 0 <= nx + dx[i] < N and visit[ny + dy[i]][nx + dx[i]] == 0 and picture[ny + dy[i]][nx + dx[i]] == c_color:
+                queue.append([nx + dx[i], ny + dy[i]])
+                visit[ny + dy[i]][nx + dx[i]] = 1 # visit을 처리해주는 부분을 새로운 길을 찾을 때 바로 해야 다른 노드에서 겹치는 길을 큐에 안담는다.
+
 for y in range(N):
     for x in range(N):
-        if (picture[y][x] == "R" or picture[y][x] == "G") and visit[y][x] == 0:
-            sss = set()
-            queue.append([x, y])
-            while queue:
-                nx, ny = queue.popleft()
-                sss.add(picture[ny][nx])
-                visit[ny][nx] = 1
-                for i in range(4):
-                    if 0 <= ny + dy[i] < N \
-                        and 0 <= nx + dx[i] < N \
-                            and visit[ny + dy[i]][nx + dx[i]] == 0 \
-                                and (picture[ny + dy[i]][nx + dx[i]] == "R" or picture[ny + dy[i]][nx + dx[i]] == "G"):
-                        queue.append([nx + dx[i], ny + dy[i]])
-            o_count += len(sss)
-            x_count += 1
-        elif picture[y][x] == "B" and visit[y][x] == 0:
-            queue.append([x, y])
-            while queue:
-                nx, ny = queue.popleft()
-                visit[ny][nx] = 1
-                for i in range(4):
-                    if 0 <= ny + dy[i] < N \
-                        and 0 <= nx + dx[i] < N \
-                            and visit[ny + dy[i]][nx + dx[i]] == 0 \
-                                and picture[ny + dy[i]][nx + dx[i]] == "B":
-                        queue.append([nx + dx[i], ny + dy[i]])
-            o_count += 1
-            x_count += 1
+        if visit[y][x] == 1:
+            continue
+        bfs(x, y)
+        o_count += 1
+
+visit = [[0] * N for _ in range(N)]
+
+for y in range(N):
+    for x in range(N):
+        if picture[y][x] == "G":
+            picture[y][x] = "R"
+
+for y in range(N):
+    for x in range(N):
+        if visit[y][x] == 1:
+            continue
+        bfs(x, y)
+        x_count += 1
 
 print(o_count, x_count)

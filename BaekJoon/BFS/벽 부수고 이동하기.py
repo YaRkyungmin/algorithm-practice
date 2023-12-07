eaf499dbf14ab314536d649b7d2287e3ba65230d
YaRkyungmin <kkgm94@naver.com>
2023-12-07 21:07:12 +0900

    ⚡️review: 벽 부수고 이동하기

--- BaekJoon/BFS/벽 부수고 이동하기.py
+++ BaekJoon/BFS/벽 부수고 이동하기.py
@@ -10,46 +10,42 @@ from collections import deque
 import copy
 
 N, M = map(int, input().rstrip().split())
-pmap = [[-1 if i == "1" else 0 for i in input().rstrip()] for _ in range(N)]
-w_list = []
-for y in range(N):
-    for x in range(M):
-        if pmap[y][x] == -1:
-            w_list.append((x,y))
-count = 100000000
-signal = False
-for i in range(-1, len(w_list)):
-    gmap = copy.deepcopy(pmap)
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    queue = deque()
-    gmap[0][0] = 1
-    queue.append((0,0))
-    if i != -1:
-        gmap[w_list[i][1]][w_list[i][0]] = 0
-    while queue:
-        nx, ny = queue.popleft()
-        if ny == N - 1 and nx == M -1:
-            count = min(gmap[ny][nx], count)
-            signal = True
-            break
+gmap = [[int(i) for i in input().rstrip()] for _ in range(N)]
+queue = deque()
+visit = [[[0, 0] for _ in range(M)] for _ in range(N)]
+visit[0][0][0] = 1
+queue.append((0, 0, 0))
+count = -1
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-        for i in range(4):
-            px = nx + dx[i]
-            py = ny + dy[i]
-            if 0 <= px < M\
-            and 0 <= py < N\
-            and gmap[py][px] == 0:
-                gmap[py][px] = gmap[ny][nx] + 1
-                queue.append((px,py))
+while queue:
+    nx, ny, broken = queue.popleft()
 
-print(count if signal else -1)
+    if ny == N - 1 and nx == M - 1:
+        count = visit[ny][nx][broken]
+        break
+    
+    for i in range(4):
+        px = nx + dx[i]
+        py = ny + dy[i]
+        if 0 <= px < M\
+        and 0 <= py < N\
+        and visit[py][px][broken] == 0:
+            if gmap[py][px] == 1 and broken == 0: #벽 일때
+                visit[py][px][1] = visit[ny][nx][0] + 1
+                queue.append((px, py, 1))
+            elif gmap[py][px] == 0: #벽 아닐때
+                visit[py][px][broken] = visit[ny][nx][broken] + 1
+                queue.append((px, py, broken))
+
+print(count)
 
 #예외
 # 011
 # 001
 # 110
-
+# (x, y, 벽뚫안뚫?)
 # 011
 # 100
 # 110
@@ -60,3 +56,10 @@ print(count if signal else -1)
 # 01010000
 # 01010011
 # 00010010
+
+# 0000000
+# 0111111
+# 0100010
+# 0101010
+# 0101010
+# 0001010

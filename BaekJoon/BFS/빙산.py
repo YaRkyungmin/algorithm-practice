1982d4705a791b2deef114ff6d6055e00160bac3
YaRkyungmin <kkgm94@naver.com>
2023-12-10 00:16:15 +0900

    ✅solved: 빙산

--- BaekJoon/BFS/빙산.py
+++ BaekJoon/BFS/빙산.py
@@ -4,3 +4,51 @@ date: 23.12.09
 title: 빙산
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N, M = map(int, input().rstrip().split())
+
+mountain = [list(map(int, input().rstrip().split())) for _ in range(N)]
+c_year = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+accept = False
+while True:
+    zero_signal = False #빙산이 있는 지 체크
+    n_mountain = [[0] * M for _ in range(N)]
+    visit = [[0] * M for _ in range(N)]
+    count = 0
+    for y in range(N):
+        for x in range(M):
+            if mountain[y][x] != 0 and visit[y][x] == 0:
+                count += 1 #빙산 수 세기
+                zero_signal = True
+                stack = []
+                stack.append((x, y))
+                visit[y][x] = 1
+                while stack:
+                    w_count = 0
+                    nx, ny = stack.pop()
+                    for i in range(4):
+                        px = nx + dx[i]
+                        py = ny + dy[i]
+                        if mountain[py][px] == 0:
+                            w_count += 1
+                        if 0 <= px < M\
+                        and 0 <= py < N\
+                        and mountain[py][px] != 0\
+                        and visit[py][px] == 0:
+                            visit[py][px] = 1
+                            stack.append((px, py))
+                    n_mountain[ny][nx] = \
+                        (mountain[ny][nx] - w_count) if (mountain[ny][nx] - w_count) >= 0 else 0 #새로운 빙산 생성
+    if zero_signal == False: #다 녹았는데 안됐을때
+        break
+    if count >= 2:
+        accept = True
+        break
+    mountain = n_mountain
+    c_year += 1
+print(c_year if accept else 0) 

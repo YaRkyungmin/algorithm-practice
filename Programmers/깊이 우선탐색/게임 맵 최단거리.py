e536059b4ef808a5e32686a719c81604522fa081
YaRkyungmin <kkgm94@naver.com>
2023-12-10 01:24:31 +0900

    ⚡️review: 게임 맵 최단거리

--- Programmers/깊이 우선탐색/게임 맵 최단거리.py
+++ Programmers/깊이 우선탐색/게임 맵 최단거리.py
@@ -4,48 +4,25 @@ date: 23.10.02
 title: 게임 맵 최단거리
 time: 30분
 """
-
-from copy import deepcopy
-
-end_x = 0
-end_y = 0
-answer = -1
-
+from collections import deque
 def solution(maps):
-    global end_x, end_y, answer
-    
-    end_x = len(maps[0]) - 1
-    end_y = len(maps) - 1
-    
-    dfs(1, maps, 0, 0)
-
-    return answer
-
-def dfs(count, game_map, x, y):
-    global end_x, end_y, answer
-
-    if x == end_x and y == end_y:
-        if answer != -1 and answer > count:
-            answer = count
-            return
-        elif answer == -1:
-            answer = count
-            return
-
-    E = [x + 1, y]
-    W = [x - 1, y]
-    N = [x, y - 1]
-    S = [x, y + 1]
-    
-    lili = [E,W,S,N]
-    new_map = deepcopy(game_map)
-
-
-    for li in lili:
-        if li[0] >= 0 and li[0] <= end_x and li[1] >= 0 and li[1] <= end_y:
-            if new_map[li[1]][li[0]] == 1:
-                count += 1
-                new_map[y][x] = 0
-                dfs(count, new_map, li[0], li[1])
-    
-    return
+    n = len(maps)
+    m = len(maps[0])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque()
+    queue.append((0,0))
+    result = -1
+    while queue:
+        nx, ny = queue.popleft()
+        if nx == (m - 1) and ny == (n - 1):
+            result = maps[ny][nx]
+        for i in range(4):
+            px = nx + dx[i]
+            py = ny + dy[i]
+            if 0 <= px < m\
+            and 0 <= py < n\
+            and maps[py][px] == 1:
+                maps[py][px] = maps[ny][nx] + 1
+                queue.append((px, py))
+    return result

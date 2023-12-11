9f46ec38971b4e32bdf5ae6f477444a44f40f6a1
YaRkyungmin <kkgm94@naver.com>
2023-12-11 15:40:11 +0900

    ❌unsolved: 숨바꼭질 3

--- BaekJoon/BFS/숨바꼭질 3.py
+++ BaekJoon/BFS/숨바꼭질 3.py
@@ -4,3 +4,33 @@ date: 23.12.11
 title: 숨바꼭질 3
 time: 30분
 """
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+queue = deque()
+queue.append((N, 0))
+visit = [10000000 for _ in range(100003)]
+visit[N] = 1
+m_time = 10000000
+while queue:
+    nn, time = queue.popleft()
+    if nn == K:
+        continue
+
+    if nn + 1 <= 100002\
+    and visit[nn + 1] > time + 1:
+        queue.append((nn + 1, time + 1))
+        visit[nn + 1] = time + 1
+        
+    if nn - 1 >- -2\
+    and visit[nn - 1] > time + 1:
+        queue.append((nn - 1, time + 1))
+        visit[nn - 1] = time + 1
+
+    if nn * 2 <= 100002\
+    and visit[nn * 2] > time:
+        queue.appendleft((nn * 2, time))
+        visit[nn * 2] = time
+print(visit[K])

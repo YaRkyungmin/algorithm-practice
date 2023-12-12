3ce6048d6f3cc21ab88725049414985ba27ee90e
YaRkyungmin <kkgm94@naver.com>
2023-12-12 18:50:22 +0900

    ❌unsolved: 곱셈

--- BaekjoonWithSwift/BaekjoonWithSwift/재귀/곱셈.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/재귀/곱셈.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 func 곱셈() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let A = input[0]
+    let B = input[1]
+    let C = input[2]
+    print(recursive(cnt: 0, total: 1))
     
+    func recursive(cnt: Int, total: Int) -> Int {
+        if cnt == B {
+            return total % C
+        }
+
+        return recursive(cnt: cnt + 1, total: (total * A) % C )
+    }
 }

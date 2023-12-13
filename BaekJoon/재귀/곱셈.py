d8e8470d2eb32135a9e8185c77c524899cb4d61b
YaRkyungmin <kkgm94@naver.com>
2023-12-13 21:02:20 +0900

    ⚡️review: 곱셈

--- BaekJoon/재귀/곱셈.py
+++ BaekJoon/재귀/곱셈.py
@@ -5,14 +5,20 @@ title: 곱셈
 time: 30분
 """
 import sys
-sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 
 A, B, C = map(int, input().rstrip().split())
 
-def recursive(cnt, total):
-    if cnt == B:
-        return total % C
-    return recursive(cnt + 1, (total * A) % C)
+def recursive(a, b, c):
+    if b == 0:
+        return 1
+    
+    tmp = recursive(a, b//2, c) % C
+    tmp = (tmp * tmp) % c
 
-print(recursive(0, 1))
+    if b % 2 == 0:
+        return tmp
+    else:
+        return (tmp * a) % c
+    
+print(recursive(A, B, C))

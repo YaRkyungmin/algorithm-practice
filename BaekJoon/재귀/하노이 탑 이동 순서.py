1a2dd50d32097e74a089b53c78f17e24d96c2871
YaRkyungmin <kkgm94@naver.com>
2023-12-14 20:58:05 +0900

    ⚡️review: 하노이 탑 이동 순서

--- BaekJoon/재귀/하노이 탑 이동 순서.py
+++ BaekJoon/재귀/하노이 탑 이동 순서.py
@@ -13,11 +13,12 @@ def hanoi(a, b, c, n):
     if n == 1:
         print(a, c)
         return
-    if n % 2 == 0:
-        hanoi(a, c, b, n - 1)
-    else:
-        hanoi(b, a, c, n - 1)
+
+    hanoi(a, c, b, n - 1)
     print(a, c)
+    hanoi(b, a, c, n - 1)
+
+print(2 ** input - 1)
 hanoi(1, 2, 3, input)
     
 # # 2 ^ n - 1

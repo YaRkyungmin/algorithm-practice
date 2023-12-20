c0ba9d0d650e4e7c67502cb444a2b3edb844eb89
YaRkyungmin <kkgm94@naver.com>
2023-12-20 14:34:18 +0900

    ❌unsolved: 최단경로

--- BaekJoon/다익스트라 알고리즘/최단경로.py
+++ BaekJoon/다익스트라 알고리즘/최단경로.py
@@ -4,3 +4,40 @@ date: 23.12.19
 title: 최단경로
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+V, E = map(int, input().rstrip().split())
+s_node = int(input().rstrip())
+graph = [[] for _ in range(V + 1)]
+D = [0 for _ in range(V + 1)]
+
+for _ in range(E):
+    u, v, w = map(int, input().rstrip().split())
+    graph[u].append((v, w))
+queue = deque()
+
+queue.append((s_node, 0))
+while queue:
+    p_node, length = queue.popleft()
+    t_dic = dict()
+    if graph[p_node]:
+        for (dn, dlength) in graph[p_node]:
+            if dn in t_dic:
+                t_dic[dn] = min(t_dic[dn], dlength)
+            else:
+                t_dic[dn] = dlength
+    for node, dlength in t_dic.items():
+        if D[node] == 0:
+            D[node] = length + dlength
+        else:
+            D[node] = min(D[node], length + dlength)
+
+        queue.append((node, D[node]))
+
+for i in range(1, V + 1):
+    if i != s_node and D[i] == 0:
+        print("INF")
+    else:
+        print(D[i])

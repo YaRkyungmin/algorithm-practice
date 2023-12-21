83d8960b2ef16a3a322a18fe561c09a5d9db4b66
YaRkyungmin <kkgm94@naver.com>
2023-12-21 18:44:40 +0900

    ✅solved: 별 찍기 11

--- BaekJoon/재귀/별 찍기 - 11.py
+++ BaekJoon/재귀/별 찍기 - 11.py
@@ -4,3 +4,23 @@ date: 23.12.21
 title: 별 찍기 - 11
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+def tri_star(n):
+    k = n//3
+    star_st = ""
+    if k == 1:
+        s_star  = ["  *  ", " * * ", "*****"]
+        return "\n".join(s_star)
+    tri_arr = tri_star(n//2).split("\n")
+    space = " " * (n//2)
+    b1_tri_arr = []
+    b2_tri_arr = []
+    for tri in tri_arr:
+        b1_tri_arr.append(space + tri + space)
+        b2_tri_arr.append(tri + " " + tri)
+    return "\n".join(b1_tri_arr + b2_tri_arr)
+print(tri_star(N))

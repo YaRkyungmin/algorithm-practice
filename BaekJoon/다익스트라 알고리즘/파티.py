9453b80495770a58ebcfff3477a57bfb854cb7a4
YaRkyungmin <kkgm94@naver.com>
2023-12-21 20:44:55 +0900

    ⚡️review: 파티

--- BaekJoon/다익스트라 알고리즘/파티.py
+++ BaekJoon/다익스트라 알고리즘/파티.py
@@ -12,38 +12,27 @@ MAX_INT = sys.maxsize
 N, M, X = map(int, input().rstrip().split())
 # vertices, edges, target point
 village = [[] for _ in range(N + 1)]
+village2 = [[] for _ in range(N + 1)]
 for _ in range(M):
     dv, av, weight = map(int, input().rstrip().split())
     village[dv].append((av, weight))
-def min_dictance(start, end):
+    village2[av].append((dv, weight))
+def min_dictance(start, graph):
     global N
     heap = []
     D = [MAX_INT for _ in range(N + 1)]
     D[start] = 0
+    D[0] = 0
     heapq.heappush(heap, (0, start))
-    m_d = 0
+
     while heap:
         weight, vertex = heapq.heappop(heap)
-        if weight == D[vertex]:
-            for (dv, dw) in village[vertex]:
-                if dv != start and dw + weight < D[dv]:
-                    D[dv] = dw + weight
-                    if dv == end:
-                        m_d = dw + weight
-                        continue
-                    heapq.heappush(heap, (dw + weight, dv))
-    return m_d
+        for (dv, dw) in graph[vertex]:
+            if dw + weight < D[dv]:
+                D[dv] = dw + weight
+                heapq.heappush(heap, (dw + weight, dv))
+    return D
 # 왕복 저장
-round_arr = [0 for _ in range(N + 1)]
-
-for x in range(1, N + 1):
-    if x == X:
-        continue
-    round_arr[x] += min_dictance(x, X)
-
-for x in range(1, N + 1):
-    if x == X:
-        continue
-    round_arr[x] += min_dictance(X, x)
-
-print(max(round_arr))
+vil1 = min_dictance(X, village)
+vil2 = min_dictance(X, village2)
+print(max([vil1[i] + vil2[i] for i in range(N + 1)]))

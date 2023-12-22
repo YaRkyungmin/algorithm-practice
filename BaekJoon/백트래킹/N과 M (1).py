e193558c60bf956f9857547a3d5c16d375133c40
YaRkyungmin <kkgm94@naver.com>
2023-12-22 18:30:30 +0900

    ❌unsolved: N과 M (1)

--- BaekJoon/백트래킹/N과 M (1).py
+++ BaekJoon/백트래킹/N과 M (1).py
@@ -4,3 +4,22 @@ date: 23.12.22
 title: N과 M (1)
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+def sequence(m, partial):
+    global N
+    if m == 1:
+        for i in range(1, N + 1):
+            if i not in partial:
+                partial.append(i)
+        return partial
+    arr = []
+    for i in range(1, N + 1):
+        arr.append(sequence(m - 1))
+        partial.append(arr)
+
+    return partial
+
+print(sequence(M, []))

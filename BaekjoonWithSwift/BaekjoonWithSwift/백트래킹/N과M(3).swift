de8ce018dec7bd19f970cdfa78b30255c9700d23
YaRkyungmin <kkgm94@naver.com>
2023-12-27 10:26:23 +0900

    ✅solved: N과 M (3)

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(3).swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(3).swift
@@ -8,5 +8,25 @@
 import Foundation
 
 func N과M3() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let M = input[1]
+    let permutation = Array(1...N)
+    var pArr = Array(repeating: 0, count: M)
+    var result = [String]()
     
+    func findPermutation(k: Int) {
+        if k == M {
+            result.append(pArr.map { String($0) }.joined(separator: " "))
+            return
+        }
+
+        for i in 0..<N {
+            pArr[k] = permutation[i]
+            findPermutation(k: k + 1)
+        }
+    }
+    
+    findPermutation(k: 0)
+    print(result.joined(separator: "\n"))
 }

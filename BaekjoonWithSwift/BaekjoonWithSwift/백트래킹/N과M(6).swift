ea9466c4bda99daa44bd960c9df264e4aeeae6c0
YaRkyungmin <kkgm94@naver.com>
2023-12-29 17:49:40 +0900

    ✅solved: N과 M (6)

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(6).swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(6).swift
@@ -8,4 +8,26 @@
 import Foundation
 
 func N과M6() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let M = input[1]
+    var numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+    numbers.sort()
+    var arr = Array(repeating: 0, count: M)
+    var result = ""
+    
+    func permutation(k: Int, max: Int) {
+        if k == M {
+            result += arr.map { String($0) }.joined(separator: " ") + "\n"
+            return
+        }
+        
+        for i in max..<N {
+            arr[k] = numbers[i]
+            permutation(k: k + 1, max: i + 1)
+        }
+    }
+    
+    permutation(k: 0, max: 0)
+    print(result)
 }

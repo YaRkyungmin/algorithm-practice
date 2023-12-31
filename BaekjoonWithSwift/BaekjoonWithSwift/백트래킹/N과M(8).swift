80ebd9198c9c26f4e089be5a461fd2fcde75a0eb
YaRkyungmin <kkgm94@naver.com>
2023-12-31 21:26:44 +0900

    ✅solved: N과 M (8)

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(8).swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(8).swift
@@ -8,3 +8,24 @@
 import Foundation
 
 func N과M8() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let M = input[1]
+    var numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+    numbers.sort()
+    var result = ""
+    
+    func permutation(k: Int, max: Int, pStr: String) {
+        if k == M {
+            result += pStr + "\n"
+            return
+        }
+        
+        for i in max..<N {
+            permutation(k: k + 1, max: i, pStr: pStr + String(numbers[i]) + " ")
+        }
+    }
+    
+    permutation(k: 0, max: 0, pStr: "")
+    print(result)
+}

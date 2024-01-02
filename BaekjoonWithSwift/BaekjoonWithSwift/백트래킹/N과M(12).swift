d439a0ebbab0628406edc876e744dd3997333989
YaRkyungmin <kkgm94@naver.com>
2024-01-02 22:58:53 +0900

    ✅solved: N과 M (12)

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(12).swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(12).swift
@@ -7,4 +7,32 @@
 
 import Foundation
 
-func N과M12() {}
+func N과M12() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let M = input[1]
+    var numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+    numbers.sort()
+    var result = ""
+    var strSet = Set<String>()
+    
+    func permutation(k: Int, max: Int, pStr: String) {
+        if k == M {
+            if strSet.contains(pStr) {
+                return
+            } else {
+                result += pStr + "\n"
+                strSet.insert(pStr)
+                return
+            }
+        }
+        
+        for i in max..<N {
+            permutation(k: k + 1, max: i, pStr: pStr + String(numbers[i]) + " ")
+            
+        }
+    }
+    
+    permutation(k: 0, max: 0, pStr: "")
+    print(result)
+}

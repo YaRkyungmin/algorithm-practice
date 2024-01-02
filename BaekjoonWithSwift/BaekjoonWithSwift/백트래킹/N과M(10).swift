14e04f833c5262838c1b1892018d282a6989cdf6
YaRkyungmin <kkgm94@naver.com>
2024-01-02 22:47:47 +0900

    ✅solved: N과 M (10)

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(10).swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/N과M(10).swift
@@ -8,5 +8,31 @@
 import Foundation
 
 func N과M10() {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let M = input[1]
+    var numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+    numbers.sort()
+    var result = ""
+    var strSet = Set<String>()
     
+    func permutation(k: Int, max: Int, pStr: String) {
+        if k == M {
+            if strSet.contains(pStr) {
+                return
+            } else {
+                result += pStr + "\n"
+                strSet.insert(pStr)
+                return
+            }
+        }
+        
+        for i in max..<N {
+            permutation(k: k + 1, max: i + 1, pStr: pStr + String(numbers[i]) + " ")
+            
+        }
+    }
+    
+    permutation(k: 0, max: 0, pStr: "")
+    print(result)
 }

57f5afcc55d849b3b866a5936689b255d1fc3fda
YaRkyungmin <kkgm94@naver.com>
2024-01-03 23:54:28 +0900

    ✅solved: 로또

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/로또.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/로또.swift
@@ -8,5 +8,35 @@
 import Foundation
 
 func 로또() {
-    
+    while true {
+        let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+        if input[0] == 0 {
+            break
+        }
+        let k = input[0]
+        var numbers = [Int]()
+        for i in 1...k {
+            numbers.append(input[i])
+        }
+        
+        var result = ""
+        var visit = Array(repeating: false, count: numbers.count)
+        
+        func lotto(t: Int, max: Int, pStr: String) {
+            if t == 6 {
+                result += pStr + "\n"
+                return
+            }
+            
+            for i in max..<k {
+                if !visit[i] {
+                    visit[i] = true
+                    lotto(t: t + 1, max: i + 1, pStr: pStr + String(numbers[i]) + " ")
+                    visit[i] = false
+                }
+            }
+        }
+        lotto(t: 0, max: 0, pStr: "")
+        print(result)
+    }
 }

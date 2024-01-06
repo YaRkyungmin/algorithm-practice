bb84876effdee34772fa24685387ca2ea926506e
YaRkyungmin <kkgm94@naver.com>
2024-01-06 21:26:20 +0900

    ⚡️review: 비숍

--- BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/비숍.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/백트래킹/비숍.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 func 비숍() {
+    // 탐색수를 줄이는 것이 핵심인 문제
     let N = Int(readLine()!)!
     var gMap = [[Int]]()
     // 비숍 놓을 수 있는 곳 1, 비숍 놓을 수 없는 곳 0
@@ -15,61 +16,48 @@ func 비숍() {
         let row = readLine()!.split(separator: " ").compactMap { Int($0) }
         gMap.append(row)
     }
-    // oneList에 1인곳을 추가해 놓음
-    var oneList = [(Int, Int)]()
+    // 대각선 oneList에 1인곳을 추가해 놓음
+    var oneList = Array(repeating: [(Int, Int)](), count: 2 * N - 1)
     for y in 0..<N {
         for x in 0..<N {
             if gMap[y][x] == 1 {
-                oneList.append((x, y))
+                oneList[x + y].append((x, y))
             }
         }
     }
     
-    var aCheckList = Array(repeating: false, count: 2 * N - 1)
-    var bCheckList = Array(repeating: false, count: 2 * N - 1)
-    var checkList = Array(repeating: false, count: oneList.count)
+    var checkList = Array(repeating: false, count: 2 * N - 1)
     var result = 0
     
-    func findBishop(max: Int) {
-        if max == checkList.count {
-            var cnt = 0
-            for i in 0..<checkList.count {
-                cnt += checkList[i] ? 1 : 0
-            }
-            result = result < cnt ? cnt : result
+    func findBishop(max: Int, count: Int) { //우상향 대각선 번호, 비숍 놓은 횟수
+        if max == 2 * N - 1{
+            result = result < count ? count : result
             return
         }
         
-        var findCount = 0
+        if oneList[max].count == 0 {
+            findBishop(max: max + 1, count: count)
+            return
+        }
         
-        for i in max..<oneList.count {
-            let node = oneList[i]
-            let x = node.0
-            let y = node.1
-            let a = x + y
-            let b = x - y + N - 1
-            if !aCheckList[a] && !bCheckList[b] && !checkList[i] {
-                findCount += 1
-                checkList[i] = true
-                aCheckList[a] = true
-                bCheckList[b] = true
-                findBishop(max: i + 1)
-                checkList[i] = false
-                aCheckList[a] = false
-                bCheckList[b] = false
+        var exeCount = 0
+        
+        for (x, y) in oneList[max] {
+            let cConstant = x - y + N - 1
+            if !checkList[cConstant] {
+                exeCount += 1
+                checkList[cConstant] = true
+                findBishop(max: max + 1, count: count + 1)
+                checkList[cConstant] = false
             }
         }
         
-        if findCount == 0 {
-            var cnt = 0
-            for i in 0..<checkList.count {
-                cnt += checkList[i] ? 1 : 0
-            }
-            result = result < cnt ? cnt : result
-            return
+        if exeCount == 0 {
+            findBishop(max: max + 1, count: count)
         }
     }
     
-    findBishop(max: 0)
+    findBishop(max: 0, count: 0)
+    
     print(result)
 }

5ad31839de6e76d004829cf0a575ce459b5c86b2
YaRkyungmin <kkgm94@naver.com>
2024-01-11 09:20:49 +0900

    ❌unsolved: 빈도 정렬

--- BaekjoonWithSwift/BaekjoonWithSwift/정렬2/빈도정렬.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/정렬2/빈도정렬.swift
@@ -8,5 +8,36 @@
 import Foundation
 
 func 빈도정렬() {
-
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let N = input[0]
+    let C = input[1]
+    var numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+    var numSet = Set<Int>()
+    var onedic = [Int: Int]() // 빈도수
+    var twodic = [Int: Int]() // 등장순서
+    var sortNumbers = [(Int, Int, Int)]()
+    
+    for i in 0..<N {
+        if !numSet.contains(numbers[i]) {
+            numSet.insert(numbers[i])
+            onedic[numbers[i]] = -1
+            twodic[numbers[i]] = i
+        } else {
+            onedic[numbers[i]] =  onedic[numbers[i]]! - 1
+        }
+    }
+    
+    for i in numSet {
+        sortNumbers.append((onedic[i]!, twodic[i]!, i))
+    }
+    
+    sortNumbers.sort { ($0.0, $0.1) < ($1.0, $1.1) }
+    
+    var result = ""
+    for (c, _, x) in sortNumbers {
+        for _ in 0..<(-c) {
+            result += String(x) + " "
+        }
+    }
+    print(result)
 }

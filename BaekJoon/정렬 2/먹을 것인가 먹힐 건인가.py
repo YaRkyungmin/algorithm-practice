3d26384dd7d92b7f499f1eb747890221b36cfaab
YaRkyungmin <kkgm94@naver.com>
2024-01-12 09:47:31 +0900

    ✅solved: 먹을 것인가 먹힐 것인가

--- BaekJoon/정렬 2/먹을 것인가 먹힐 건인가.py
+++ BaekJoon/정렬 2/먹을 것인가 먹힐 건인가.py
@@ -4,3 +4,21 @@ date: 24.1.12
 title: 먹을 것인가 먹힐 것인가
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+T = int(input().rstrip())
+for _ in range(T):
+    N, M = map(int, input().rstrip().split())
+    a_list = list(map(int, input().rstrip().split()))
+    b_list = list(map(int, input().rstrip().split()))
+
+    a_list.sort()
+    b_list.sort()
+    pointer = 0
+    sum = 0
+    for i in a_list:
+        while pointer < M and i > b_list[pointer]:
+            pointer += 1
+        sum += pointer
+    print(sum)

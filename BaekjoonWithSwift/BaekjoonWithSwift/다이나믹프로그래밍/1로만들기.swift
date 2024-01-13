993f6501e09356f921c23609959c4c836cee1bde
YaRkyungmin <kkgm94@naver.com>
2024-01-13 21:50:06 +0900

    ⚡️review: 1로 만들기

--- BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/1로만들기.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/1로만들기.swift
@@ -9,31 +9,26 @@ import Foundation
 
 func 일로만들기() {
     let N = Int(readLine()!)!
-    var dp = Array(repeating: 100000, count: N + 1)
-    
+    var dp = [Int: Int]()
     dp[1] = 0
-    
-    if N > 1 {
-        findMin(m: 2)
-    }
-    // bottom up
-    func findMin(m: Int) {
-        if m % 2 == 0 && m % 3 == 0 {
-            dp[m] = min(dp[m/3] + 1 , dp[m/2] + 1, dp[m-1] + 1)
-        } else if m % 2 == 0 {
-            dp[m] = min(dp[m/2] + 1, dp[m-1] + 1)
+    // top_down
+    func findMin(m: Int) -> Int {
+        if dp.keys.contains(m) {
+            return dp[m]!
+        }
+        
+        if m % 3 == 0 && m % 2 == 0 {
+            dp[m] = min(findMin(m: m / 3) + 1, findMin(m: m / 2) + 1)
         } else if m % 3 == 0 {
-            dp[m] = min(dp[m/3] + 1, dp[m-1] + 1)
+            dp[m] = min(findMin(m: m / 3) + 1, findMin(m: m - 1) + 1)
+        } else if m % 2 == 0 {
+            dp[m] = min(findMin(m: m / 2) + 1, findMin(m: m - 1) + 1)
         } else {
-            dp[m] = dp[m-1] + 1
+            dp[m] = findMin(m: m - 1) + 1
         }
         
-        if m == N {
-            return
-        }
-        
-        findMin(m: m + 1)
+        return dp[m]!
     }
     
-    print(dp[N])
+    print(findMin(m: N))
 }

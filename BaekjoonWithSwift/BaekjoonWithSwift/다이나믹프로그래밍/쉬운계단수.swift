16b16728295c6d572bc63aa7f94d48ecc490289d
YaRkyungmin <kkgm94@naver.com>
2024-01-16 18:27:59 +0900

    ❌unsolved: 쉬운 계단 수

--- BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/쉬운계단수.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/쉬운계단수.swift
@@ -8,4 +8,12 @@
 import Foundation
 
 func 쉬운계단수() {
+    let N = Int(readLine()!)!
+    var dp = Array(repeating: 0, count: N)
+    dp[0] = 9
+    for i in 1..<N {
+        dp[i] = (dp[i - 1] * 2 - i) % 1000000000
+    }
+    
+    print(dp[N - 1])
 }

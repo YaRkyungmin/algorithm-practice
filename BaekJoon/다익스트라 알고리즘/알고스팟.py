f4e7f49fab0febf7bd93617bfbc2197eebd89786
YaRkyungmin <kkgm94@naver.com>
2024-01-18 03:09:33 +0900

    ⚡️review: 알고스팟

--- BaekJoon/다익스트라 알고리즘/알고스팟.py
+++ BaekJoon/다익스트라 알고리즘/알고스팟.py
@@ -15,22 +15,15 @@ for _ in range(N):
     row = list(map(int, input().rstrip()))
     graph.append(row)
 
-for y in range(N):
-    for x in range(M):
-        if graph[y][x] == 1:
-            graph[y][x] = 2
-        else:
-            graph[y][x] = 1
-
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 dist = [[MAX_INT for _ in range(M)] for _ in range(N)]
 dist[0][0] = 0
-heap = [(0, 0, 0, 0)]
+heap = [(0, 0, 0)]
 dis_count = 0
 
 while heap:
-    c, x, y, distance_count = heapq.heappop(heap)
+    c, x, y = heapq.heappop(heap)
     if dist[y][x] < c:
         continue
     for i in range(4):
@@ -40,9 +33,7 @@ while heap:
         and 0 <= py < N\
         and dist[py][px] > (c + graph[py][px]):
             cost = c + graph[py][px]
-            heapq.heappush(heap, (cost, px, py, distance_count + 1))
-            dist[py][px] = c + cost
-            if py == N - 1 and px == M - 1:
-                dis_count = distance_count + 1
+            heapq.heappush(heap, (cost, px, py))
+            dist[py][px] = cost
 
-print((dist[N - 1][M - 1]) - dis_count)
+print((dist[N - 1][M - 1]))

3a390fefa59e36a7f2cc9278ea1630d44887340e
YaRkyungmin <kkgm94@naver.com>
2024-01-18 21:45:18 +0900

    ✅solved: 2×n 타일링 2

--- BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/2xn타일링2.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/2xn타일링2.swift
@@ -8,4 +8,15 @@
 import Foundation
 
 func 이n타일링투() {
+    let n = Int(readLine()!)!
+    
+    var dp = Array(repeating: 0, count: n)
+    dp[0] = 1
+    var signal = 1
+    
+    for i in 1..<n {
+        dp[i] = (dp[i-1]*2 + signal) % 10007
+        signal = signal == 1 ? -1 : 1
+    }
+    print(dp[n-1])
 }

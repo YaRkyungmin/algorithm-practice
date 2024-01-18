a668b2886b70a9e44cc760ab276c8350b4fd6224
YaRkyungmin <kkgm94@naver.com>
2024-01-19 02:32:03 +0900

    ✅solved: 연속합 with Python

--- BaekJoon/다이나믹 프로그래밍/연속합.py
+++ BaekJoon/다이나믹 프로그래밍/연속합.py
@@ -6,3 +6,20 @@ time: 30분
 """
 import sys
 input = sys.stdin.readline
+
+n = int(input().rstrip())
+numbers = list(map(int, input().rstrip().split()))
+
+dp = [0 for _ in range(n)]
+dp[0] = numbers[0]
+max_result = dp[0]
+if n > 1:
+    for x in range(1, n):
+        if dp[x - 1] > 0:
+            dp[x] = numbers[x] + dp[x - 1]
+        else:
+            dp[x] = numbers[x]
+        
+        max_result = max(dp[x], max_result)
+
+print(max_result)

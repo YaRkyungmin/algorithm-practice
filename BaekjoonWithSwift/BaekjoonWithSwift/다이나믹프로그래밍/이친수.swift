a16403a98d46815d8dc0323b86209f4008b2acc9
YaRkyungmin <kkgm94@naver.com>
2024-01-18 22:28:01 +0900

    ✅solved: 이친수

--- BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/이친수.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/이친수.swift
@@ -8,4 +8,21 @@
 import Foundation
 
 func 이친수() {
+    let N = Int(readLine()!)!
+    
+    var dp = Array(repeating: 0, count: N)
+    dp[0] = 1
+    var numbers = (0, 1)
+    
+    for i in 1..<N {
+        var newNum = (0, 0)
+        
+        newNum.0 += numbers.0
+        newNum.1 += numbers.0
+        newNum.0 += numbers.1
+        dp[i] = newNum.0 + newNum.1
+        numbers = newNum
+    }
+    
+    print(dp[N-1])
 }

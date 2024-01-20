590ebfb0a8b70b96fb356f95990a2277e5664f4f
YaRkyungmin <kkgm94@naver.com>
2024-01-20 16:10:59 +0900

    ✅solved: 가장 큰 증가하는 부분

--- BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/가장큰증가하는부분수열.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/가장큰증가하는부분수열.swift
@@ -8,4 +8,23 @@
 import Foundation
 
 func 가장큰증가하는부분수열() {
+    let N = Int(readLine()!)!
+    let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+    var dp = Array(repeating: 0, count: N)
+    dp[0] = numbers[0]
+    var result = dp[0]
+    
+    for i in 1..<N {
+        let fix = numbers[i]
+        var sum = 0
+        for x in (0..<i).reversed() {
+            if fix > numbers[x] {
+                sum = max(dp[x], sum)
+            }
+        }
+        dp[i] = sum + fix
+        result = max(dp[i], result)
+    }
+    
+    print(result)
 }

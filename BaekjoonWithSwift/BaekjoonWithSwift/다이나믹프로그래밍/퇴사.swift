2b68fadf83b11cda1f226d6d191ae96de7be595c
YaRkyungmin <kkgm94@naver.com>
2024-01-23 01:40:00 +0900

    ❌unsolved: 퇴사

--- BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/퇴사.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/다이나믹프로그래밍/퇴사.swift
@@ -8,5 +8,35 @@
 import Foundation
 
 func 퇴사() {
+    let N = Int(readLine()!)!
+    var consult = [(Int, Int)]()
+    for _ in 0..<N {
+        let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+        consult.append((input[0], input[1]))
+    }
+    var dp = Array(repeating: 0, count: N)
+    for i in (0..<N).reversed() {
+        let (d, w) = consult[i]
+        
+        if i + d - 1 >= N {
+            continue
+        }
+        
+        var sum = 0
+        
+        for x in i...(i + d - 1) {
+            sum += dp[x]
+        }
+        
+        if sum < w {
+            dp[i] = w - sum
+        }
+    }
+    var result = 0
     
+    for i in 0..<N {
+        result += dp[i]
+    }
+    print(result)
 }
+

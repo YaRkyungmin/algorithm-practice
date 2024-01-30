4f3808973f26d26abcbb7a5d97e603a7943d0a52
YaRkyungmin <kkgm94@naver.com>
2024-01-30 11:07:09 +0900

    ✅solved: RGB거리

--- BaekJoon/다이나믹 프로그래밍/RGB거리.py
+++ BaekJoon/다이나믹 프로그래밍/RGB거리.py
@@ -4,3 +4,17 @@ date: 24.1.30
 title: RGB거리
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+rgb = [list(map(int, input().rstrip().split())) for _ in range(N)]
+dp = [[0, 0 , 0] for _ in range(N)]
+dp[0] = rgb[0]
+for y in range(1, N):
+    dp[y][0] = min(dp[y - 1][1] + rgb[y][0], dp[y - 1][2] + rgb[y][0])
+    dp[y][1] = min(dp[y - 1][0] + rgb[y][1], dp[y - 1][2] + rgb[y][1])
+    dp[y][2] = min(dp[y - 1][0] + rgb[y][2], dp[y - 1][1] + rgb[y][2])
+
+print(min(dp[N - 1]))

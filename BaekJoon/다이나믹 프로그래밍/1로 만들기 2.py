68f54ccd23d2322bd8a5b606f0fa238a0b016ebd
YaRkyungmin <kkgm94@naver.com>
2024-01-31 14:18:09 +0900

    ⚡️review: 1로 만들기 2

--- BaekJoon/다이나믹 프로그래밍/1로 만들기 2.py
+++ BaekJoon/다이나믹 프로그래밍/1로 만들기 2.py
@@ -9,76 +9,24 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-dp = [[0, []] for _ in range(N + 1)]
+dp = [0 for _ in range(N + 1)]
+history = [0 for _ in range(N + 1)]
 
-def find_min_idx(dist):
-    idx = 0
-    m_value = dist[0]
-    for i in range(1, len(dist)):
-        if dist[i] < m_value:
-            idx = i
-            m_value = dist[i]
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1
+    history[i] = i - 1
 
-    return idx, m_value
-
-def mkr(idx, i):
-    if idx == 0:
-        return i // 3
-    elif idx == 1:
-        return i // 2
-    else:
-        return i - 1
+    if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
+        dp[i] = dp[i // 3] + 1
+        history[i] = i // 3
+    if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
+        dp[i] = dp[i // 2] + 1
+        history[i] = i // 2
     
-def mkr1(idx, i):
-    if idx == 0:
-        return i // 3
-    else:
-        return i - 1
-
-def mkr2(idx, i):
-    if idx == 0:
-        return i // 2
-    else:
-        return i - 1
-
-dp[1][1] = [1]
-
-if N >= 2:
-    dp[2][0] = 1
-    dp[2][1] = [1, 2]
-if N >= 3:
-    dp[3][0] = 1
-    dp[3][1] = [1, 3]
-
-if N > 3:
-    for i in range(4, N + 1):
-        if i % 3 == 0 and i % 2 == 0:
-            li = [dp[i // 3][0], dp[i // 2][0], dp[i - 1][0]]
-            idx, m_value = find_min_idx(li)
-            dp[i][0] = m_value + 1
-            ali = list(dp[mkr(idx, i)][1])
-            ali.append(i)
-            dp[i][1] = ali
-        elif i % 3 == 0:
-            li = [dp[i // 3][0], dp[i - 1][0]]
-            idx, m_value = find_min_idx(li)
-            dp[i][0] = m_value + 1
-            ali = list(dp[mkr1(idx, i)][1])
-            ali.append(i)
-            dp[i][1] = ali
-        elif i % 2 == 0:
-            li = [dp[i // 2][0], dp[i - 1][0]]
-            idx, m_value = find_min_idx(li)
-            dp[i][0] = m_value + 1
-            ali = list(dp[mkr2(idx, i)][1])
-            ali.append(i)
-            dp[i][1] = ali
-        else:
-            dp[i][0] = dp[i - 1][0] + 1
-            ali = list(dp[i - 1][1])
-            ali.append(i)
-            dp[i][1] = ali
-    
-
-print(dp[N][0])
-print(*sorted(dp[N][1], reverse = True))
+print(dp[N])
+dist = []
+p = N
+while p != 0:
+    dist.append(p)
+    p = history[p]
+print(*dist)

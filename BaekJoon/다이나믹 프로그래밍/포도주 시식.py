6b9067ea965e06049285395aefeb05a1611dc50f
YaRkyungmin <kkgm94@naver.com>
2024-02-02 16:35:22 +0900

    ✅solved: 포도주 시식

--- BaekJoon/다이나믹 프로그래밍/포도주 시식.py
+++ BaekJoon/다이나믹 프로그래밍/포도주 시식.py
@@ -4,3 +4,24 @@ date: 24.2.2
 title: 포도주 시식
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+juice = []
+for _ in range(n):
+    juice.append(int(input().rstrip()))
+
+dp = [0 for _ in range(n)]
+dp[0] = juice[0]
+
+if n > 1:
+    dp[1] = juice[0] + juice[1]
+
+if n > 2:
+    dp[2] = max(juice[0] + juice[1], juice[0] + juice[2], juice[1] + juice[2])
+
+    for i in range(3, n):
+        dp[i] = max(dp[i - 2] + juice[i], dp[i - 1], dp[i - 3] + juice[i - 1] + juice[i])
+
+print(dp[n - 1])

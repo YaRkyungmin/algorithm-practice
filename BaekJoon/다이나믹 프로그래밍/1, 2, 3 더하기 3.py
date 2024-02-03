f39ff4f6b4bebbbe89fe37afeec0cee963a7c21a
YaRkyungmin <kkgm94@naver.com>
2024-02-03 23:05:20 +0900

    ✅solved: 1, 2, 3 더하기 3

--- BaekJoon/다이나믹 프로그래밍/1, 2, 3 더하기 3.py
+++ BaekJoon/다이나믹 프로그래밍/1, 2, 3 더하기 3.py
@@ -4,3 +4,23 @@ date: 24.2.3
 title: 1, 2, 3 더하기 3
 time: 30분
 """
+
+import sys
+input = sys.stdin.readline
+
+T = int(input().rstrip())
+arr = []
+max_num = 0
+
+for _ in range(T):
+    n = int(input().rstrip())
+    max_num = max(n, max_num)
+    arr.append(n)
+
+dp = [0, 0, 1] + [0 for _ in range(max_num)]
+
+for i in range(3, max_num + 3):
+    dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1000000009
+
+for i in arr:
+    print(dp[i + 2])

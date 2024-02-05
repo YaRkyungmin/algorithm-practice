98ccbebed4a140ad9fc37445d9cc352c64a70fd1
YaRkyungmin <kkgm94@naver.com>
2024-02-05 11:40:35 +0900

    ✅solved: 카드 구매하기

--- BaekJoon/다이나믹 프로그래밍/카드 구매하기.py
+++ BaekJoon/다이나믹 프로그래밍/카드 구매하기.py
@@ -6,3 +6,12 @@ time: 30분
 """
 import sys
 input = sys.stdin.readline
+
+N = int(input().rstrip())
+numbers = list(map(int, input().rstrip().split())) # 1 5 6 7
+dp = list(numbers)
+for i in range(1, N):
+    for j in range(i - 1, -1, -1):
+        dp[i] = max(dp[i], dp[j] + numbers[i - j - 1])
+
+print(dp[N - 1])

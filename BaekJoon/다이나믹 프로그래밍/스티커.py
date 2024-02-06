a318050d594e148562e0e883b4d1a0540647bb8c
YaRkyungmin <kkgm94@naver.com>
2024-02-07 02:45:26 +0900

    ✅solved: 스티커

--- BaekJoon/다이나믹 프로그래밍/스티커.py
+++ BaekJoon/다이나믹 프로그래밍/스티커.py
@@ -4,3 +4,21 @@ date: 24.2.6
 title: 스티커
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+T = int(input().rstrip())
+
+for _ in range(T):
+    n = int(input().rstrip())
+    numbers = [[0] + list(map(int, input().rstrip().split())) for _ in range(2)]
+    
+    dp = [[0 for _ in range(n + 1)] for _ in range(2)]
+    dp[0][1] = numbers[0][1]
+    dp[1][1] = numbers[1][1]
+
+    for i in range(2, n + 1):
+        dp[0][i] = max(dp[1][i - 1], dp[0][i - 2], dp[1][i - 2]) + numbers[0][i]
+        dp[1][i] = max(dp[0][i - 1], dp[0][i - 2], dp[1][i - 2]) + numbers[1][i]
+    
+    print(max(dp[0][n], dp[1][n]))

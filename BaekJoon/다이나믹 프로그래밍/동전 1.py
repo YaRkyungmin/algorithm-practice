e94089eb1b65259617e4c854f2402072b8b59cf6
YaRkyungmin <kkgm94@naver.com>
2024-02-08 23:41:26 +0900

    ❌unsolved: 동전 1

--- BaekJoon/다이나믹 프로그래밍/동전 1.py
+++ BaekJoon/다이나믹 프로그래밍/동전 1.py
@@ -7,3 +7,12 @@ time: 30분
 import sys
 input = sys.stdin.readline
 
+n, k = map(int, input().rstrip().split())
+coins = [int(input().rstrip()) for _ in range(n)]
+dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+for c in coins:
+    for i in range(c, k + 1):
+        dp[i] += dp[i - c]
+
+print(dp[k])

50a12a59f091d94db7c7264fc9ce7c3dd7002074
YaRkyungmin <kkgm94@naver.com>
2024-02-11 01:36:54 +0900

    ❌unsolved: 제곱수의 합

--- BaekJoon/다이나믹 프로그래밍/제곱수의 합.py
+++ BaekJoon/다이나믹 프로그래밍/제곱수의 합.py
@@ -4,3 +4,18 @@ date: 24.2.11
 title: 제곱수의 합
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+MAX_INT = sys.maxsize
+
+N = int(input().rstrip())
+dp = [0, 1] + [MAX_INT for _ in range(1, N)]
+
+for i in range(1, N + 1):
+    if i**(1/2) - int(i**(1/2)) == 0:
+        dp[i] = 1
+        continue
+    for j in range(int(i/2), i):
+        dp[i] = min(dp[j] + dp[i - j], dp[i])
+
+print(dp[N])

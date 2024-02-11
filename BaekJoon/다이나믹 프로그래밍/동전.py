3b8bd2db999f4b1f2e7f354a2108f4a761749cdb
YaRkyungmin <kkgm94@naver.com>
2024-02-12 01:05:20 +0900

    ✅solved: 동전

--- BaekJoon/다이나믹 프로그래밍/동전.py
+++ BaekJoon/다이나믹 프로그래밍/동전.py
@@ -4,3 +4,18 @@ date: 24.2.12
 title: 동전 
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+T = int(input().rstrip())
+
+for _ in range(T):
+    N = int(input().rstrip())
+    coins = list(map(int, input().rstrip().split()))
+    M = int(input().rstrip())
+    dp = [1] + [0 for _ in range(M)]
+
+    for coin in coins:
+        for i in range(coin, M + 1):
+            dp[i] += dp[i - coin]
+    print(dp[M])

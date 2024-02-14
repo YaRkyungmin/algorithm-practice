6a88e76e6cb554cefd0f4a959fa8d8a7a3c4a7a8
YaRkyungmin <kkgm94@naver.com>
2024-02-15 02:14:41 +0900

    ❌unsolved: 돌 게임

--- BaekJoon/다이나믹 프로그래밍/돌 게임.py
+++ BaekJoon/다이나믹 프로그래밍/돌 게임.py
@@ -4,3 +4,13 @@ date: 24.2.15
 title: 돌 게임
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+if N < 4:
+    print("SK")
+else:
+    print("CY" if N % 4 == 0 else "SK")
+        

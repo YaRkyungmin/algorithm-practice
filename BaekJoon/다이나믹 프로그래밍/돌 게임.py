868fbb11d435d00eaba5ee81402ef745225ada08
YaRkyungmin <kkgm94@naver.com>
2024-02-15 02:55:59 +0900

    ⚡️review: 돌 게임

--- BaekJoon/다이나믹 프로그래밍/돌 게임.py
+++ BaekJoon/다이나믹 프로그래밍/돌 게임.py
@@ -9,8 +9,22 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-if N < 4:
-    print("SK")
-else:
-    print("CY" if N % 4 == 0 else "SK")
+print("CY" if N % 2 == 0 else "SK")
         
+
+# win = [-1]*10001
+
+# win[1] = 1 #SK
+# win[2] = 0 #CY
+# win[3] = 1 #SK
+
+# for i in range(4,n+1):
+#     if win[i-1] == 1 or win[i-3] == 1:
+#         win[i] = 0
+#     else:
+#         win[i] = 1
+
+# if win[n]==1:
+#     print('SK')
+# else:
+#     print('CY')

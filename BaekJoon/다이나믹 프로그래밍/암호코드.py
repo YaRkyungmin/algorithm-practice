34bf766a0aafe1965503520fcf62d516ab65f7f1
YaRkyungmin <kkgm94@naver.com>
2024-02-16 14:37:42 +0900

    ⚡️review: 암호코드

--- BaekJoon/다이나믹 프로그래밍/암호코드.py
+++ BaekJoon/다이나믹 프로그래밍/암호코드.py
@@ -16,14 +16,25 @@ if code[0] == 0:
     signal = False
 else:
     for i in range(1, len(code)):
+        rs = True
         if code[i] == 0:
-            signal = False
-            break
-        dp[i][0] = dp[i - 1][0] + dp[i - 1][1]
-
-        if code[i - 1] * 10 + code[i] <= 26:
-            dp[i][1] = dp[i - 1][0]
+            if 0 < code[i - 1] * 10 + code[i] <= 20: # 중간에 0이 들어간 예외
+                dp[i][0] = dp[i - 1][0]
+                rs = False
+            else:
+                signal = False
+                break
+        if rs:
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1]
+            if code[i - 1] == 0:
+                dp[i][0] = dp[i - 1][0]
+            else:
+                if code[i - 1] * 10 + code[i] <= 26:
+                    dp[i][1] = dp[i - 1][0]
 if signal:
     result = dp[len(code) - 1][0] + dp[len(code) - 1][1]
+if len(code) == 1\
+and signal:
+    result = 1
 
-print(result)
+print(result % 1000000)

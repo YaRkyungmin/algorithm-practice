51101e4effbe658647c3bc55589af2abd7845572
YaRkyungmin <kkgm94@naver.com>
2024-02-17 17:11:14 +0900

    ⚡️review: 동전 2

--- BaekJoon/다이나믹 프로그래밍/동전 2.py
+++ BaekJoon/다이나믹 프로그래밍/동전 2.py
@@ -10,15 +10,12 @@ input = sys.stdin.readline
 n, k = map(int, input().rstrip().split())
 coins = [int(input().rstrip()) for _ in range(n)]
 coins.sort()
-dp = [0 for _ in range(k + 1)]
+dp = [10001 for _ in range(k + 1)]
 dp[0] = 0
 
 for coin in coins:
     for i in range(1, k + 1):
-        if i - coin == 0:
-            dp[i] = 1
-        elif i - coin > 0:
-            if dp[i - coin] != 0:
-                dp[i] = dp[i - coin] + 1 if dp[i] == 0 else min(dp[i - coin] + 1, dp[i])
+        if i - coin >= 0:
+            dp[i] = min(dp[i - coin] + 1, dp[i])
 
-print(-1 if dp[k] == 0 else dp[k])
+print(-1 if dp[k] == 10001 else dp[k])

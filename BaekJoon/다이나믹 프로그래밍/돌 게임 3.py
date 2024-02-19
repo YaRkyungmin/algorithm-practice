735f84136c61359b469b13ba4be3956457b45579
YaRkyungmin <kkgm94@naver.com>
2024-02-20 04:52:14 +0900

    ✅solved: 돌 게임 3

--- BaekJoon/다이나믹 프로그래밍/돌 게임 3.py
+++ BaekJoon/다이나믹 프로그래밍/돌 게임 3.py
@@ -5,3 +5,17 @@ title: 돌 게임 3
 time: 30분
 """
 import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+dp = [0, 1, 0, 1, 1] + [0 for _ in range(N - 4)]
+
+for i in range(5, N + 1):
+    if dp[i - 1] == 0\
+    or dp[i - 3] == 0\
+    or dp[i - 4] == 0:
+        dp[i] = 1
+
+print("SK" if dp[N] == 1 else "CY")
+
+# print('CY' if int(input())%7 in (0,2) else 'SK')

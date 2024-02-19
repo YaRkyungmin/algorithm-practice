1837f161ee0110063008fad0ccc4bd7cdf06445d
YaRkyungmin <kkgm94@naver.com>
2024-02-19 23:33:45 +0900

    ❌unsolved: 내리막 길

--- BaekJoon/다이나믹 프로그래밍/내리막 길.py
+++ BaekJoon/다이나믹 프로그래밍/내리막 길.py
@@ -4,3 +4,34 @@ date: 24.2.19
 title: 내리막 길
 time: 30분
 """
+## DFS와 DP 를 이용한 재귀 풀이도 무조건 풀어보자!
+import sys
+input = sys.stdin.readline
+import heapq
+
+M, N = map(int, input().rstrip().split())
+graph = [list(map(int, input().rstrip().split())) for _ in range(M)]
+
+heap = [(-graph[0][0], 0, 0)]
+dx = [0, 0, 1, -1]
+dy = [-1, 1, 0, 0]
+
+dp = [[0 for _ in range(N)] for _ in range(M)]
+dp[0][0] = 1
+
+while heap:
+    h, x, y = heapq.heappop(heap)
+    for i in range(4):
+        px = x + dx[i]
+        py = y + dy[i]
+        
+        if 0 <= px < N\
+        and 0 <= py < M\
+        and graph[py][px] < -h:
+            if dp[py][px] != 0:
+                dp[py][px] += dp[y][x]
+            else:
+                dp[py][px] += dp[y][x]
+                heapq.heappush(heap,(-graph[py][px], px, py))
+
+print(dp[M - 1][N - 1])
--- README.md
+++ README.md
@@ -234,4 +234,4 @@
 |2월 16일| [다이나믹 프로그래밍 - 암호코드](https://www.acmicpc.net/problem/2011) |❌|✅❌|
 |2월 17일| [다이나믹 프로그래밍 - 동전 2](https://www.acmicpc.net/problem/2294) |✅|✅❌|
 |2월 18일| [다이나믹 프로그래밍 - 타일 채우기](https://www.acmicpc.net/problem/2133) |❌|✅❌|
-|2월 19일| [다이나믹 프로그래밍 - 내리막 길](https://www.acmicpc.net/problem/1520) |✅❌|✅❌|
+|2월 19일| [다이나믹 프로그래밍 - 내리막 길](https://www.acmicpc.net/problem/1520) |❌|✅❌|

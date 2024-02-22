2fe98ae7bb893a3add7708d914293568400ec16f
YaRkyungmin <kkgm94@naver.com>
2024-02-22 23:11:01 +0900

    ❌unsolved: 수 고르기

--- BaekJoon/투 포인터/수 고르기.py
+++ BaekJoon/투 포인터/수 고르기.py
@@ -6,4 +6,27 @@ time: 30분
 """
 import sys
 input = sys.stdin.readline
+MAX_INT = sys.maxsize
 
+N, M = map(int, input().rstrip().split())
+numbers = [int(input().rstrip()) for _ in range(N)]
+numbers.sort()
+s = 0
+e = 0
+result = MAX_INT
+while e < N:
+    patial_result = numbers[e] - numbers[s]
+    if patial_result < M:
+        e += 1
+    elif patial_result == M:
+        result = M
+        break
+    else:
+        s += 1
+        result = min(patial_result, result)
+
+print(result)
+        
+
+    
+    

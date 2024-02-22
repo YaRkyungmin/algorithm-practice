7c0209352a4c9b1b2d08485e6e77544e1408ce73
YaRkyungmin <kkgm94@naver.com>
2024-02-23 00:14:57 +0900

    ❌unsolved: 부분합

--- BaekJoon/투 포인터/부분합.py
+++ BaekJoon/투 포인터/부분합.py
@@ -4,3 +4,28 @@ date: 24.2.23
 title: 부분합
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+MAX_INT = sys.maxsize
+
+N, S = map(int, input().rstrip().split())
+numbers = list(map(int, input().rstrip().split()))
+s = 0
+e = 0
+result = MAX_INT
+sum_numbers = numbers[0]
+signal = False
+while e < N - 1 or s <= e:
+    if sum_numbers < S:
+        if e == N - 1:
+            break
+        e += 1
+        sum_numbers += numbers[e]
+    else:
+        signal = True
+        result = min(result, e - s + 1)
+        sum_numbers -= numbers[s]
+        s += 1
+
+print(result if signal else 0)
+    

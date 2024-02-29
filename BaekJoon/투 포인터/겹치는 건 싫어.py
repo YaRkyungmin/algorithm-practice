139efdbb7ead8d71655e72d155ffd1335d10576a
YaRkyungmin <kkgm94@naver.com>
2024-02-29 21:58:27 +0900

    ✅solved: 겹치는 건 싫어

--- BaekJoon/투 포인터/겹치는 건 싫어.py
+++ BaekJoon/투 포인터/겹치는 건 싫어.py
@@ -4,3 +4,23 @@ date: 24.2.29
 title: 겹치는 건 싫어
 time: 30분
 """
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+numbers = list(map(int, input().rstrip().split()))
+st = 0
+en = 0
+visited = [0 for _ in range(1000001)]
+result = 0
+
+while en < N:
+    if visited[numbers[en]] < K: # 방문한적이 K 보다 작을때
+        visited[numbers[en]] += 1
+        en += 1
+    else:
+        result = max(result, en - st)        
+        visited[numbers[st]] -= 1
+        st += 1
+result = max(result, en - st)
+print(result)
--- README.md
+++ README.md
@@ -244,4 +244,4 @@
 |2월 26일| [투 포인터 - List of Unique Numbers](https://www.acmicpc.net/problem/13144) |❌|✅❌|
 |2월 27일| [투 포인터 - 가장 긴 짝수 연속한 부분 수열 (large)](https://www.acmicpc.net/problem/22862) |✅|✅❌|
 |2월 28일| [투 포인터 - 회전 초밥](https://www.acmicpc.net/problem/2531) |❌|✅❌|
-|2월 29일| [투 포인터 - 겹치는 건 싫어](https://www.acmicpc.net/problem/20922) |✅❌|✅❌|
+|2월 29일| [투 포인터 - 겹치는 건 싫어](https://www.acmicpc.net/problem/20922) |✅||

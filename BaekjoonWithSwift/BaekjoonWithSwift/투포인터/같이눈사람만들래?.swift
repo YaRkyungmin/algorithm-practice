c0660a86b43e046abca4c8d6c07b3f5deb905124
YaRkyungmin <kkgm94@naver.com>
2024-04-22 18:55:35 +0900

    ⚡️review: 같이 눈사람 만들래?

--- BaekjoonWithSwift/BaekjoonWithSwift/투포인터/같이눈사람만들래?.swift
+++ BaekjoonWithSwift/BaekjoonWithSwift/투포인터/같이눈사람만들래?.swift
@@ -12,7 +12,9 @@ func 같이눈사람만들래() {
     var numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
     numbers.sort()
     var result = 1000000001
+    var signal = false
     for i in stride(from: 0, to: N - 3, by: 1) {
+        if signal { break }
         for j in stride(from: i + 3, to: N, by: 1) {
             let A = numbers[i] + numbers[j]
             var st = i + 1
@@ -28,9 +30,11 @@ func 같이눈사람만들래() {
                     en -= 1
                 } else {
                     result = 0
+                    signal = true
                     break
                 }
             }
+            if signal { break }
         }
     }
     print(result)

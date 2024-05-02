0747c28db766e61943fa4d1590f86bf6eead0047
YaRkyungmin <kkgm94@naver.com>
2024-05-02 09:58:03 +0900

    ⚡️review: 2xn 타일링

--- BaekJoon/동적 계획법/2×n 타일링.py
+++ BaekJoon/동적 계획법/2×n 타일링.py
@@ -1,24 +1,16 @@
 """
 author: kyungmin
-date: 23.08.31
+date: 23.08.31 & 24.05.02
 title: 2×n 타일링
 time: 30분
 """
-
 import sys
 input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-dp = [0] * (N + 1)
-
-dp[0] = 1
-dp[1] = 1
-
-
+dp = [1, 1] + [0 for _ in range(N - 1)]
 for i in range(2, N + 1):
-    if i == 2:
-        dp[2] = 2
-    else:
-        dp[i] = 2 * dp[i-1] - dp[i-3]
-print(dp[N] % 10007)
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+
+print(dp[N])

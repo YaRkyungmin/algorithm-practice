a3be11e55dfc7922834410697aa68b042dad8ead
YaRkyungmin <kkgm94@naver.com>
2024-07-09 10:24:40 +0900

    ✅solved: 게임을 만든 동준이

--- BaekJoon/그리디/게임을 만든 동준이.py
+++ BaekJoon/그리디/게임을 만든 동준이.py
@@ -6,3 +6,15 @@ time: 30분
 """
 import sys
 input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+numbers = [int(input().rstrip()) for _ in range(N)]
+m = numbers[N - 1]
+result = 0
+for i in range(N - 2, -1, -1):
+    while m <= numbers[i]:
+        numbers[i] -= 1
+        result += 1 
+    m = numbers[i]
+print(result)

8b2bd7cecae9b486d876258b20c99202c0f75eff
YaRkyungmin <kkgm94@naver.com>
2024-09-23 02:19:38 +0900

    ⚡️review: 섬 연결하기

--- Programmers/그리디/섬 연결하기.py
+++ Programmers/그리디/섬 연결하기.py
@@ -4,15 +4,40 @@ date: 23.09.08
 title: 섬 연결하기
 time: 30분
 """
+from heapq import *
 def solution(n, costs):
-    answer = 0
-    dp = [False] * n
-    costs.sort(key = lambda x: x[2]) 
+    for i in range(len(costs)):
+        costs[i] = [costs[i][2], costs[i][0], costs[i][1]]
+    visit = [True for _ in range(n)]
+    if len(costs) == 1:
+        return 0
+    count = 2
+    heapify(costs)
+    S, a, b = heappop(costs)
+    visit[a] = False
+    visit[b] = False
+    answer = S
 
-    for i in costs:
-        if dp[i[0]] and dp[i[1]]:
-            continue
-        dp[i[0]] = True
-        dp[i[1]] = True
-        answer += i[2]
+    while count < n:
+        stack = []
+        signal = True
+        while signal:
+            S, a, b = heappop(costs)
+            if visit[a] and visit[b] == False:
+                signal = False        
+                count += 1
+                answer += S
+                visit[a] = False
+            elif  visit[b] and visit[a] == False:
+                signal = False
+                count += 1
+                answer += S
+                visit[b] = False
+            if signal:
+                stack.append([S, a, b])
+        while stack:
+            heappush(costs, stack.pop())
+
     return answer
+
+print(solution(4, [[0,1,1],[0,2,2],[1,2,1],[1,3,1],[2,3,8]]))

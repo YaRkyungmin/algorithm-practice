9f73226a747963f8a5f171123b0bc12f0d79b062
YaRkyungmin <kkgm94@naver.com>
2024-10-28 20:50:10 +0900

    ❌unsolved: 합분해

--- BaekJoon/다이나믹 프로그래밍/합분해.py
+++ BaekJoon/다이나믹 프로그래밍/합분해.py
@@ -4,3 +4,15 @@ date: 24.10.28
 title: 합분해
 time: 30분
 """
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N, K = map(int, input().split())
+
+dp = [[1] * (N + 1) for _ in range(K)]
+
+for k in range(1, K):
+    for n in range(1, N + 1):
+        dp[k][n] = dp[k][n - 1] + dp[k - 1][n]
+
+print(dp[K - 1][N] % 1000000000)
